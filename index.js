20acf7c46d6dea876209fabccb77eaf36f452ba9
Mjawad12 <workingofficial156@gmail.com>
2023-09-20 04:53:54 -0700

    update

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const express = require("express");
 const user = require("./Routes/Users");
-const db = require("./ConnectToDb");
+const db = require("mongoose");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 var cors = require("cors");
 const reservation = require("./Routes/reservation");
 const Port = 5000;
-
 const app = express();
 app.use(cors());
-db();
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("launched /");
 });
-
 app.use("/api/", user);
 app.use("/api/reserv", reservation);
 
-app.listen(process.env.PORT || Port, () => {
-  console.log("app launched on port 5500");
-});
+const connectToDb = () => {
+  db.connect(process.env.Connection_string)
+    .then(() => {
+      console.log("Connected to Db");
+      app.listen(process.env.PORT || Port, () => {
+        console.log("app launched on port 5500");
+      });
+    })
+    .catch((error) => {
+      console.log("Can not Connecy to Db" + error);
+    });
+};
+
+connectToDb();
