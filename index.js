aa1e16e3a669b8989df33f0f2fa052cd79a7ecfa
Mjawad12 <workingofficial156@gmail.com>
2023-09-20 05:51:11 -0700

    update

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const express = require("express");
-const user = require("./Routes/Users");
-const db = require("mongoose");
+const app = express();
+const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 var cors = require("cors");
 const reservation = require("./Routes/reservation");
+const user = require("./Routes/Users");
 
-const app = express();
+const ConnectTodb = async () => {
+  try {
+    mongoose.connect(process.env.Connection_string);
+    console.log("Connected");
+  } catch (error) {
+    console.log("Can not connect to db" + error.message);
+    process.exit(1);
+  }
+};
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("launched /");
 });
+
 app.use("/api/", user);
 app.use("/api/reserv", reservation);
 
-db.connect(
-  "mongodb+srv://SwiftRentals_Main:RentalSwift156jaw@swiftrentals.pm3mdfi.mongodb.net/"
-)
+ConnectTodb()
   .then(() => {
     console.log("Connected to Db");
     app.listen(process.env.PORT || 3000, () => {
-      console.log("app launched on port 5500");
+      console.log("app launched on port 3000");
     });
   })
   .catch((error) => {
